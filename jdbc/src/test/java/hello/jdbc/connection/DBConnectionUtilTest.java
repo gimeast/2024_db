fb4a13f4c52c92e050265c5bdc75861626148c0d
gimeast <dudu.gimeast@gmail.com>
2024-04-15 23:05:04 +0900

    스프링 DB 1편 - 데이터 접근 핵심 원리
    - 데이터베이스 연결

---
+++ jdbc/src/test/java/hello/jdbc/connection/DBConnectionUtilTest.java
@@ -0,0 +1,22 @@
+package hello.jdbc.connection;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+
+import static org.assertj.core.api.Assertions.*;
+
+
+@Slf4j
+class DBConnectionUtilTest {
+    
+    @Test
+    @DisplayName("db 연결")
+    void connection() {
+        Connection connection = DBConnectionUtil.getConnection();
+        assertThat(connection).isNotNull();
+    }
+    
+}
