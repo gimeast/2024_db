27b6e4dcc74bd7873ed8c55f64d89bf63f9bdee1
gimeast <dudu.gimeast@gmail.com>
2024-04-16 23:47:26 +0900

    스프링 DB 1편 - 데이터 접근 핵심 원리
    - JDBC 개발 - 조회

--- jdbc/src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
+++ jdbc/src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
@@ -9,6 +9,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.NoSuchElementException;
 
 /**
  * JDBC - DriverManager 사용
@@ -18,7 +19,6 @@ public class MemberRepositoryV0 {
 
     public Member save(Member member) throws SQLException {
         String sql = "insert into member(member_id, money) values(?, ?)";
-
         Connection con = null;
         PreparedStatement pstmt = null;
 
@@ -35,8 +35,32 @@ public class MemberRepositoryV0 {
         } finally {
             close(con, pstmt, null);
         }
+    }
 
+    public Member findById(String memberId) throws SQLException {
+        String sql = "select * from member where member_id = ?";
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
 
+        try {
+            con = getConnection(); //커넥션 획듯
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, memberId);
+            rs = pstmt.executeQuery();
+
+            if (rs.next()) {
+                return new Member(rs.getString("member_id"), rs.getInt("money"));
+            } else {
+                throw new NoSuchElementException("member not found memberId = " + memberId);
+            }
+
+        } catch (SQLException e) {
+            log.error("db error", e);
+            throw e;
+        } finally {
+            close(con, pstmt, rs);
+        }
     }
 
     private void close(Connection con, Statement stmt, ResultSet rs) {
