6d6c613df953a1f92ff3be8c61577a2f883c9da6
gimeast <dudu.gimeast@gmail.com>
2024-04-17 23:07:33 +0900

    스프링 DB 1편 - 데이터 접근 핵심 원리
    - JDBC 개발 - 수정, 삭제

--- jdbc/src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
+++ jdbc/src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
@@ -3,10 +3,13 @@ package hello.jdbc.repository;
 import hello.jdbc.domain.Member;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.sql.SQLException;
+import java.util.NoSuchElementException;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 @Slf4j
 class MemberRepositoryV0Test {
@@ -15,12 +18,24 @@ class MemberRepositoryV0Test {
 
     @Test
     void crud () throws SQLException {
-        Member member = new Member("memberV5", 10000);
+        //생성
+        Member member = new Member("memberV1", 10000);
         repository.save(member);
 
+        //조회
         Member findMember = repository.findById(member.getMemberId());
         log.info("findMember: {}", findMember);
         assertThat(findMember).isEqualTo(member);
+
+        //수정
+        repository.update(findMember.getMemberId(), 50000);
+        Member findMember2 = repository.findById(findMember.getMemberId());
+        log.info("findMember2: {}", findMember2);
+        assertThat(findMember).isNotEqualTo(findMember2);
+
+        //삭제
+        repository.delete(findMember2.getMemberId());
+        assertThrows(NoSuchElementException.class, () -> repository.findById(findMember2.getMemberId())).printStackTrace();
     }
 
 }
