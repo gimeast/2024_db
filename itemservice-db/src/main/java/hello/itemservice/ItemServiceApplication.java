93e18bac51064c0ab8dcd3049725f4e9c36918e2
gimeast <dudu.gimeast@gmail.com>
2024-05-29 22:35:03 +0900

    스프링 DB 2편 - 데이터 접근 활용 기술
    - 데이터 접근 기술 - 테스트
      - 테스트 - 임베디드 모드 DB

--- itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -2,13 +2,17 @@ package hello.itemservice;
 
 import hello.itemservice.config.*;
 import hello.itemservice.repository.ItemRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
+import javax.sql.DataSource;
 
+@Slf4j
 //@Import(MemoryConfig.class)
 //@Import(JdbcTemplateV1Config.class)
 //@Import(JdbcTemplateV2Config.class)
@@ -26,4 +30,18 @@ public class ItemServiceApplication {
 		return new TestDataInit(itemRepository);
 	}
 
+	@Bean
+	@Profile("test")
+	public DataSource dataSource() {
+		log.info("메모리 데이터베이스 초기화");
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("org.h2.Driver");
+		//애플리케이션 실행시 JVM에 데이터베이스를 만들고 데이터를 쌓는다.
+		//jdbc:h2:mem:db는 임베디드 모드로 동작하는 H2 데이터베이스를 사용할 수 있다.
+		//DB_CLOSE_DELAY=-1는 임베디드 모드에서는 데이터베이스 커넥션 연결이 모두 끊어지면 데이터베이스도 종료되는데 그것을 방지하는 설정이다.
+		dataSource.setUrl("jdbc:h2:mem:db;DB_CLOSE_DELAY=-1");
+		dataSource.setUsername("sa");
+		dataSource.setPassword("");
+		return dataSource;
+	}
 }
