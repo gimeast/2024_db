0f9910de8599777a219f066076277e1227e5088f
gimeast <dudu.gimeast@gmail.com>
2024-06-09 13:55:35 +0900

    스프링 DB 2편 - 데이터 접근 활용 기술
    - 스프링 트랜잭션 이해
      - 트랜잭션 적용 확인

---
+++ springtx/src/test/java/study/springtx/apply/TxBasicTest.java
@@ -0,0 +1,61 @@
+package study.springtx.apply;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.support.AopUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@Slf4j
+@SpringBootTest
+public class TxBasicTest {
+
+    @Autowired
+    BasicService basicService;
+
+    @TestConfiguration
+    static class TxApplyBasicConfig {
+        @Bean
+        BasicService basicService() {
+            return new BasicService();
+        }
+    }
+
+    @Slf4j
+    static class BasicService {
+
+        @Transactional
+        public void tx() {
+            log.info("call tx");
+            boolean txActive = TransactionSynchronizationManager.isActualTransactionActive(); //트랜잭션 적용 여부 확인하는 기능
+            log.info("tx active is {}", txActive);
+        }
+
+        public void nonTx() {
+            log.info("call nonTx");
+            boolean txActive = TransactionSynchronizationManager.isActualTransactionActive(); //트랜잭션 적용 여부 확인하는 기능
+            log.info("tx active is {}", txActive);
+        }
+    }
+
+    @Test
+    void proxyCheck() {
+        log.info("aop class = {}", basicService.getClass());
+        assertThat(AopUtils.isAopProxy(basicService)).isTrue();
+    }
+
+    @Test
+    void txTest() {
+        basicService.tx();
+        basicService.nonTx();
+    }
+
+
+}
