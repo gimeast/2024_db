0df0365cb468e4bda2c6abd6a8dfb4f60faa9c13
gimeast <dudu.gimeast@gmail.com>
2024-06-12 21:58:50 +0900

    스프링 DB 2편 - 데이터 접근 활용 기술
    - 스프링 트랜잭션 전파1 - 기본
      - 스프링 트랜잭션 전파1 - 커밋, 롤백

---
+++ springtx/src/test/java/study/springtx/propagation/BasicTxTest.java
@@ -0,0 +1,52 @@
+package study.springtx.propagation;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.interceptor.DefaultTransactionAttribute;
+
+import javax.sql.DataSource;
+
+@Slf4j
+@SpringBootTest
+public class BasicTxTest {
+
+    @Autowired
+    PlatformTransactionManager transactionManager;
+
+    @TestConfiguration
+    static class Config {
+
+        //spring boot가 기본적으로 트랜잭션 매니저를 생성해주지만 이렇게 정의하면 정의한 트랜잭션 매니저를 사용한다.
+        @Bean
+        public PlatformTransactionManager transactionManager(DataSource dataSource) {
+            return new DataSourceTransactionManager(dataSource);
+        }
+    }
+
+    @Test
+    void commit() {
+        log.info("트랜잭션 시작");
+        TransactionStatus status = transactionManager.getTransaction(new DefaultTransactionAttribute());
+
+        log.info("트랜잭션 커밋 시작");
+        transactionManager.commit(status);
+        log.info("트랜잭션 커밋 완료");
+    }
+
+    @Test
+    void rollback() {
+        log.info("트랜잭션 시작");
+        TransactionStatus status = transactionManager.getTransaction(new DefaultTransactionAttribute());
+
+        log.info("트랜잭션 롤백 시작");
+        transactionManager.rollback(status);
+        log.info("트랜잭션 롤백 완료");
+    }
+}
